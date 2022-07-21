9076beed6793cb3298de416c3717633f1e76c8b8
ondrejspok <ondrej.spok@gmail.com>
2022-07-21 17:44:27 +0200

    phonebook 3.1

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end('Hellow world')
-})
+const http = require("http");
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server runnng on port ${PORT}`)
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-Type": "text/plain" });
+//   response.end("Server created");
+// });
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    content: body.content,
+
+    date: new Date(),
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server runnng on port ${PORT}`);
