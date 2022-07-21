030452ea2bb52d9e86a771812ba2538374e93e3f
ondrejspok <ondrej.spok@gmail.com>
2022-07-21 18:52:54 +0200

    3.4: Phonebook backend step4

--- index.js
+++ index.js
@@ -41,6 +41,12 @@ app.get("/api/info", (request, response) => {
   response.send(`Phonebook has info for ${persons.length} people / ${dateNow}`);
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+  })
+
 const generateId = () => {
   const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
   return maxId + 1;
