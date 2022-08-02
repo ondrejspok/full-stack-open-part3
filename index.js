d4674ad0ebf7b0446eac23682ad72abdecae5d0e
ondrejspok <ondrej.spok@gmail.com>
2022-08-02 10:42:42 +0200

    3.16: Phonebook database, step4

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
+const person = require("./models/person");
 
 app.use(cors());
 app.use(express.static("build"));
@@ -46,10 +47,16 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/person/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.get("/api/person/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -102,13 +109,25 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
